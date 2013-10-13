97fbc77e3d8d6fcb64e3d04460d425f7f1e539d7
Adam Beerman <adam.beerman@gmail.com>
2013-10-13 08:42:51 -0400

    adding c_users per class notes

---
+++ controllers/c_users.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: adam
+ * Date: 10/13/13
+ * Time: 8:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+<?php
+class users_controller extends base_controller {
+
+    public function __construct() {
+        parent::__construct();
+        echo "users_controller construct called<br><br>";
+    }
+
+    public function index() {
+        echo "This is the index page";
+    }
+
+    public function signup() {
+        echo "This is the signup page";
+    }
+
+    public function login() {
+        echo "This is the login page";
+    }
+
+    public function logout() {
+        echo "This is the logout page";
+    }
+
+    public function profile($user_name = NULL) {
+
+        if($user_name == NULL) {
+            echo "No user specified";
+        }
+        else {
+            echo "This is the profile for ".$user_name;
+        }
+    }
+
+} # end of the class
+?>
