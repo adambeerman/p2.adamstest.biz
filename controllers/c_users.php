d96992c941194bdb0990b8cb0d5a4504e88e1431
Adam Beerman <adam.beerman@gmail.com>
2013-10-14 09:25:06 -0400

    reverting changes to c_users

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: adam
- * Date: 10/13/13
- * Time: 8:39 AM
- * To change this template use File | Settings | File Templates.
- */
 class users_controller extends base_controller {
 
     public function __construct() {
@@ -29,24 +22,14 @@ class users_controller extends base_controller {
         echo "This is the logout page";
     }
 
-    public function profile($user_name == NULL) {
+    public function profile($user_name = NULL) {
 
-        /*
-        If you look at _v_template you'll see it prints a $content variable in the <body>
-        Knowing that, let's pass our v_users_profile.php view fragment to $content so
-        it's printed in the <body> */
-
-        $this->template->content = View::instance('v_users_profile');
-
-        # $title is another variable used in _v_template to set the <title> of the page
-        $this->template->title = "Profile";
-
-        # Pass information to the view fragment
-        $this->template->content->user_name = $user_name;
-
-        # Render View
-        echo $this-template;
-
+        if($user_name == NULL) {
+            echo "No user specified";
+        }
+        else {
+            echo "This is the profile for ".$user_name;
+        }
     }
 
 } # end of the class
