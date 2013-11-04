a9b48ec6eb176f27991cfa447ba4212b2725927d
Adam Beerman <adam.beerman@gmail.com>
2013-11-04 16:29:35 -0500

    corrected heading error on landing page

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -8,17 +8,16 @@
 
 </h1>
 
-<h4>
-    <?=APP_NAME?> is a micro-blog that allows you to make posts and follow your friends!
-    <br><br>
-    +1 Features include:
-    <ul>
-        <li>Editing Posts</li>
-        <li>Deleting Posts</li>
-        <li>Uploading a Profile Photo</li>
-    </ul>
+<?=APP_NAME?> is a micro-blog that allows you to make posts and follow your friends!
+<br><br>
++1 Features include:
+<ul>
+    <li>Editing Posts</li>
+    <li>Deleting Posts</li>
+    <li>Uploading a Profile Photo</li>
+</ul>
 
-    <br><br>
-    Revenge is a dish best served cold. But <?=APP_NAME?> is hot! We're glad you could join us.
-</h4>
+<br><br>
+Revenge is a dish best served cold. But <?=APP_NAME?> is hot! We're glad you could join us.
+
 
